2efeb12bac7d8fca8567d276b98b0341a82c08b3
Eduardo Stuart <hi@s.tuart.me>
2019-03-28 19:33:26 +0100

    fix - missing env eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    node: true,
   },
   extends: 'airbnb-base',
   globals: {
@@ -11,5 +12,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-  }
+  },
+  rules: {
+  },
 };
